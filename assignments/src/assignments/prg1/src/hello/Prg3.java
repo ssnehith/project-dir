e2e1d4162391504801121e6fe824129608e43929
ssnehith <55785794+ssnehith@users.noreply.github.com>
2019-10-16 09:44:54 +0530

    Add files via upload

---
+++ assignments/src/assignments/prg1/src/hello/Prg3.java
@@ -0,0 +1,20 @@
+package hello;
+
+import java.util.Arrays;
+
+
+
+public class Prg3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i< args.length;i++)
+			
+		{
+			Arrays.sort(args);
+	    
+	    System.out.println(args[i]);
+	    }
+	}
+
+}
